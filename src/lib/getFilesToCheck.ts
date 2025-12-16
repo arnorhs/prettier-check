eaeb5fd9b3d79440625d7c9fa8b9dc16fca4fa6b
arnorhs <arnorhs@gmail.com>
2025-12-16 10:42:02 +0000

    feat: omit deleted from git diff to not invoke errors on prettier not finding removed files (#3)

--- src/lib/getFilesToCheck.ts
+++ src/lib/getFilesToCheck.ts
@@ -2,7 +2,7 @@ import { exec } from './exec'
 
 export async function getFilesToCheck(ref: string) {
   try {
-    const { stdout } = await exec(`git diff --name-only ${ref}`)
+    const { stdout } = await exec(`git diff --diff-filter=d --name-only ${ref}`)
 
     return stdout.trim()
   } catch (e: any) {
